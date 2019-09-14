b1c955684d5af85301912268cadfe7c5e66c0759
PollyCR <polly.c.reynolds@gmail.com>
2019-09-14 20:17:31 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector('main#main').remove()
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = `✨Polly is the champion✨` 
